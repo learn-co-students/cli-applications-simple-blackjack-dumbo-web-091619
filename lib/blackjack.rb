7d65e4d28647e7f0d6ac9264cf65d29e05da837c
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-09-09 14:17:21 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,23 +1,40 @@
 def welcome
-  # code #welcome here
+do
+  it "prints a welcome message to screen" do
+    expect($stdout).to receive(:puts).with("Welcome to the Blackjack Table")
+    welcome
+  end
 end
 
 def deal_card
-  # code #deal_card here
+  do
+  it "generates a random number between 1-11" do
+    20.times do 
+      card = deal_card
+      expect(card).to be > 0
+      expect(card).to be < 12
+    end
+  end
 end
 
 def display_card_total
-  # code #display_card_total here
-end
-
+  do
+  it "accepts one argument, the card total" do
+    expect { display_card_total(7) }.to_not raise_error
+  end
 def prompt_user
   # code #prompt_user here
 end
 
 def get_user_input
-  # code #get_user_input here
+   do
+  it "returns the value of a `gets.chomp` method" do
+    ["h", "s", "exit"].each do |string|
+      expect(self).to receive(:gets).and_return(string)
+      expect(get_user_input).to eq(string)
+    end
+  end
 end
-
 def end_game
   # code #end_game here
 end
